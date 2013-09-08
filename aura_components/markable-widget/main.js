cede3aad6f15735212a6e3eecea54ea04123d5d1
yapcheahshen <yapcheahshen@gmail.com>
2013-09-08 23:24:30 +0800

    use backbone collection

--- aura_components/markable-widget/main.js
+++ aura_components/markable-widget/main.js
@@ -1,60 +1,26 @@
-define(['underscore','backbone',
+define(['underscore','backbone','aem',
 'text!./markable.tmpl'], 
-  function(_,Backbone,template) {
+  function(_,Backbone,aem,template) {
   return {
     type: 'Backbone',
     events: {
-      "click tk":"clicktoken",
-
-      "mousemove tk":"mousemove"
+      "click tk":"clickdomnode",
     },
-
-    vposfromtoken:function(tk) {
+    vposfromdomnode:function(tk) {
       var token=tk.attributes['n'].value;
       var slot=tk.parentNode.attributes['n'].value;
       return parseInt(slot)*4096+parseInt(token) ;
     },
-    tokenfromvpos:function(vpos) {
+    domnodefromvpos:function(vpos) {
         var slot=Math.floor(vpos /4096);
         var $slot=this.slot2dom[slot];
         if (!$slot) $slot=this.slot2dom[slot]=this.$el.find('slot[n='+slot+']');
         return $slot.find('tk[n='+vpos %4096+']');
     },
-    mousemove:function(e) {
-      if (e.target.nodeName!='TK') return;
-      var vpos=this.vposfromtoken(e.target);
-      if (vpos==this.lastvpos) return;
-      this.lastvpos=vpos;
-    },
-    tillendofsentence:function($tk) {
-      var o="";
-      var tk=$tk[0];
-      while (tk) {
-        if (tk.nodeType==3) o+=tk.textContent;
-        else o+=tk.innerHTML;
-        tk=tk.nextSibling;
-      }
-      return o.replace(/<.*?>/g,'');
-    },
-    clicktoken: function(e) {
+    clickdomnode: function(e) {
       if (this.readonly) return;
       var $tk=this.$el.find(e.target), $slot=$tk.parent(); 
-      
-      var vpos=this.vposfromtoken(e.target);
-      
-      var tag=this.model.get("tag");
-      var that=this;
-      if ($tk.hasClass(tag)) {
-        this.removetag(function() {
-          that.model.set("selected",$tk);
-          $tk.removeClass();
-          that.model.set("selected",null);
-        });
-      } else {
-        $tk.removeClass(); 
-        this.model.set("selected",$tk);        
-        this.addtag();
-      }
+      this.toggletag($tk);
     },    
     settext:function(id,text) {
       if (id!=this.id) return;
@@ -62,9 +28,6 @@ define(['underscore','backbone',
     },
     setselectable:function() {
       this.readonly=false;
-    },
-    loadparallel:function() {
-      if (this.options.load) this.sandbox.emit('parallel.load',this.db, this.start);
     },
     setheight:function() {
       var screenheight=parseInt($(".mainview").css('height'),10)-parseInt($("#controlpanel").css('height'),10);
@@ -79,69 +42,76 @@ define(['underscore','backbone',
       }
       return attrs;
     },
-    removetag:function(callback) {
-      var selected=this.model.get("selected");
-      if (!selected) return ;
-      var cls=selected.attr('class');
-      var attrs=this.tagdataattributes(selected,cls);
-      if (attrs.length) {
-        bootbox.confirm('Remove <span class="'+cls+'">'+cls+
-          "</span> in "+selected.html()+" with attribute:<b>"
-          +selected.attr(attrs[0])+"</b>?",function(res){
-          if (res) {
-            callback();
-          }
-        })
-      } else {
-        callback();
-      }
+    toggletag:function(selected) {
+      var vpos=this.vposfromdomnode(selected[0]);
+      var tag=this.model.get('tag');
+      var m=this.aemCollection.findWhere({tagname:tag,vpos:vpos});
+      if (m) this.removeaem(m);
+      else this.addtag(selected);
     },
-    addtag:function() {
-      var selected=this.model.get("selected");
+    addtag:function(selected) {
       if (!selected) return ;
+      var that=this;
+      var vpos=this.vposfromdomnode(selected[0]);
       var tag=this.model.get('tag');
       var taginfo=this.model.get('taginfo');
-
       if (taginfo&&taginfo.data) {
         bootbox.prompt('input '+taginfo.data+' for '+selected.html(),function(result){
           if (result) {
-            selected.addClass(tag);
-            selected.attr('data-'+tag+'-'+taginfo.data,result);
+            var extra={};
+            extra[taginfo.data]=result;
+            that.aemCollection.add({id:tag+vpos,vpos:vpos, tagname:tag, extra: extra})
           }
         });
       } else {
-        selected.addClass(tag);
+        this.aemCollection.add({id:tag+vpos,vpos:vpos, tagname:tag })
       }
-        
-
     },
-    model:new Backbone.Model(),
-    tagchanged:function() {
-      //this.removetag();
-      //this.addtag();
+    clearaem:function() {
+      this.$el.find("tk").removeClass();
+    },
+    renderaem:function() {
+      this.clearaem();
+      var C=this.aemCollection.models;
+      for (var i in C){
+        this.newaem(C[i]);
+      }
+    },
+    removeaem:function(m) {
+      var vpos=m.get("vpos");
+      var node=this.domnodefromvpos(vpos);
+      var tag=node.attr("class");
+      node.removeClass();
+      this.aemCollection.remove(m)
+    },
+    newaem:function(m) {
+      var node=this.domnodefromvpos(m.get("vpos"));
+      if (!node) return;
+      var tag=m.get("tagname");
+      node.addClass(m.get("tagname"));  
+      var extra=m.get("extra");
+      for (var i in extra) {
+        node.attr('data-'+tag+'-'+i,extra[i]);
+      }
     },
     settag:function(tag,taginfo) {
       this.model.set("tag",tag);
       this.model.set("taginfo",taginfo);
     },
-    getAEM:function() {
-
-    },
-    setAEM:function() {
-
-    },
     initialize: function() {
       this.readonly=false;
+      this.model=new Backbone.Model();
       this.slot2dom={}; //for speed up tokenfromvpos
       this.setheight();
       this.id=this.options.id;
+      this.aemCollection=new aem.AEMCollection();
+      this.aemCollection.on("remove",this.removeaem,this);
+      this.aemCollection.on("add",this.newaem,this);
+      this.aemCollection.on("reset",this.renderaem,this);
       this.sandbox.on("parallel.setselectable",this.setselectable,this);
       this.sandbox.on('markable.settext',this.settext,this);
       this.sandbox.on('markable.settag',this.settag,this);
-      this.sandbox.on("AEM.get",this.getAEM,this);
-      this.sandbox.on("AEM.set",this.setAEM,this);
       this.model.on("change:tag",this.tagchanged,this);
-      this.loadparallel();      
     }
   };
 });
